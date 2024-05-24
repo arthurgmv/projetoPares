41d6625d220ccebc60c662db5477bb91d4b275e0
Arthur Gabriel <arthurgabrielmv@gmail.com>
2024-05-24 08:35:07 -0300

    Initial commit

---
+++ src/Pares.java
@@ -0,0 +1,15 @@
+public class Pares {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 100; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+            ;
+        }
+        ;
+        String letters = "Hello World";
+        for (int i = 0; i <= letters.length() - 1; i++){
+            System.out.println(letters.charAt(i) + " ");
+        }
+    }
+}
